57f5043c96b044cdd6caca479d5f7397c669760d
messyKassaye <nurelayinmehammed@gmail.com>
2020-02-17 15:28:14 +0300

    latest commit

--- src/authentication/admin/components/AdminHome.js
+++ src/authentication/admin/components/AdminHome.js
@@ -2,16 +2,55 @@ import React, {Component} from 'react';
 import {Container} from "@material-ui/core";
 import {Grid} from "@material-ui/core";
 import AdminCard from "./widgets/AdminCard";
-import {green} from "@material-ui/core/colors";
+import {deepOrange, deepPurple, green} from "@material-ui/core/colors";
+import CommonDashboardCard from "../../commons/components/CommonDashboardCard";
 
 class AdminHome extends Component {
     render() {
         return (
             <Container maxWidth='lg'>
                 <Grid container spacing={2}>
+
                     <Grid item md={3} xs={12}>
-                        <AdminCard content={[{name:'Drivers',value:200},{name:'Advertisers',value:200}]} type='users' backgroundColor={green[500]} title={'Total users'} route={'/auth/admin/users'}/>
+                        <CommonDashboardCard
+                            chartBackgroundColor={deepOrange[500]}
+                            cardBackgroundColor={green[500]}
+                            textColor={'white'}
+                            title={'23K'}
+                            subheader={'Total users'}
+                        />
                     </Grid>
+
+                    <Grid item md={3} xs={12}>
+                        <CommonDashboardCard
+                            chartBackgroundColor={green[500]}
+                            cardBackgroundColor={'#3C4252'}
+                            textColor={'white'}
+                            title={'23K'}
+                            subheader={'Total adverts'}
+                        />
+                    </Grid>
+
+                    <Grid item md={3} xs={12}>
+                        <CommonDashboardCard
+                            chartBackgroundColor={deepPurple[500]}
+                            cardBackgroundColor={deepOrange[500]}
+                            textColor={'white'}
+                            title={'23K'}
+                            subheader={'Total advert deposit'}
+                        />
+                    </Grid>
+
+                    <Grid item md={3} xs={12}>
+                        <CommonDashboardCard
+                            chartBackgroundColor={deepPurple[500]}
+                            cardBackgroundColor={deepOrange[500]}
+                            textColor={'white'}
+                            title={'23K'}
+                            subheader={'Total advert deposit'}
+                        />
+                    </Grid>
+
                 </Grid>
             </Container>
         );
---
+++ src/authentication/commons/components/CommonDashboardCard.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import {Card, CardContent, CardHeader} from "@material-ui/core";
+import {
+    AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip,
+} from 'recharts';
+import {chartData as data} from "../../admin/data/chartData";
+
+class CommonDashboardCard extends Component {
+    render() {
+        return (
+            <Card style={{backgroundColor:this.props.cardBackgroundColor,color:this.props.textColor}}>
+                <CardHeader
+                 title={this.props.title}
+                 subheader={this.props.subheader}
+                />
+                <CardContent style={{padding:0}}>
+                    <AreaChart
+                        width={260}
+                        height={50}
+                        data={data}
+                        margin={{top:0,left:0,right:0,bottom:0}}
+                    >
+                        <Area type="monotone" dataKey="uv" stroke={this.props.chartBackgroundColor} fill={this.props.chartBackgroundColor} />
+                    </AreaChart>
+
+                </CardContent>
+
+            </Card>
+        );
+    }
+}
+
+export default CommonDashboardCard;
